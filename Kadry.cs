f8ed93cd70ac2ab031aa865561de7f4fb510db66
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-02-20 12:08:58 +0300

    num 3

---
+++ Kadry.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac24._3
+{
+    internal class Kadry
+    {
+        /// <summary>
+        /// поля
+        /// </summary>
+        string fam;
+        int age;
+        string dol;
+        int staj;
+        /// <summary>
+        /// Инкапсуляция
+        /// </summary>
+        public string Fam
+        {
+            get { return fam; }
+            set => fam = value;
+        }
+        public int Age
+        {
+            get { return age; }
+            set { if (value < 16) { age = 16; } else if (value > 60) { age = 60; } else { age = value; } }
+        }
+        public string Dol
+        {
+            get { return dol; }
+            set => dol = value;
+        }
+        public int Staj
+        {
+            get { return staj; }
+            set { if (value < 0) { staj = 0; } else if (value > 45) { staj = 45; } else { staj = value; } }
+
+        }
+        /// <summary>
+        /// Без параметров
+        /// </summary>
+        public Kadry() { }
+        /// <summary>
+        /// С параметрами
+        /// </summary>
+        /// <param name="fam"></param>
+        /// <param name="age"></param>
+        /// <param name="dol"></param>
+        /// <param name="staj"></param>
+        public Kadry(string fam, int age, string dol, int staj)
+        {
+            this.Fam = fam;
+            this.Age = age;
+            this.Dol = dol;
+            this.Staj = staj;
+        }
+        /// <summary>
+        /// метод для вывода значений об объекте.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Фамилия = {Fam}, Возраст = {Age}, Должность = {Dol}, Стаж = {Staj}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace prac24._3
     {
         static void Main(string[] args)
         {
+            //объект с именем kadry,конструктор без параметров
+            Kadry kadry = new Kadry();
+            Console.WriteLine(kadry.ToString());
+            //объект с именем kadry2,конструктор c параметрами
+            Kadry kadry2 = new Kadry("Крикун", 70, "Лейтенант", 46);
+            Console.WriteLine(kadry2.ToString());
+            //конструктор с parse
+            Console.WriteLine("Введите фамилию");
+            string fam = Console.ReadLine();
+            Console.WriteLine("Введите возраст");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите должность");
+            string dol = Console.ReadLine();
+            Console.WriteLine("Введите стаж");
+            int staj = int.Parse(Console.ReadLine());
+            Kadry kadry3 = new Kadry(fam, age, dol, staj);
+            Console.WriteLine($"{kadry3.ToString()}\n");
+            Console.ReadLine();
         }
     }
 }
